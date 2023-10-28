3fecdf3fa8000536a4a791e8515948de7e08b6f0
andoh <solomonkingsman@gmail.com>
2023-10-28 00:54:55 +0000

    initial commit

--- dictionary.py
+++ dictionary.py
@@ -3,8 +3,27 @@ people = {
     'htanaka': 'Haru Tanaka',
     'ppatel': 'Priya Pate',
     'bagarcia': 'Benjamin Albert Garcia',
-    'zimn': 'Zhang Min',
+    'zmin': 'Zhang Min',
     'afarooqi': 'Ayesha farooq',
 }
 # print(people)
 print(people['bagarcia'])
+
+# get length of dictionary
+# len(dictionary name)
+how_many = len(people)
+print(how_many)
+print(len(people))
+
+
+# use the in keyword to search for a key in dictionary
+if 'jajackson' in people:
+    print('jajackson can be found')
+else:
+    print('sorry buddy!:( ')
+# or
+print('jajackson' in people)
+print('zmin' in people)
+
+# get dictionary data
+# dictionary name.get(key)
